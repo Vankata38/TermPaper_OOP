87569c1bd2a623d83de78548070b5e44f334506f
Ivan Terziev <ivanterziev38@gmail.com>
2023-10-20 22:55:27 +0300

    Add project files.

---
+++ TermPaper_OOP/MainWinow.cs
@@ -0,0 +1,10 @@
+namespace TermPaper_OOP
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TermPaper_OOP/VectorObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermPaper_OOP
+{
+    public abstract class VectorObject
+    {
+        private float X { get; set; }
+        private float Y { get; set; }
+
+        public VectorObject(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public abstract void Perimeter();
+        public abstract void Area();
+        public abstract void Draw();
+    }
+}
