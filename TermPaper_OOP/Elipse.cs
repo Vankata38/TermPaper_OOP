44ba519bfbaae9d366e991cc5ef319e176bb955b
Ivan Terziev <ivanterziev38@gmail.com>
2023-10-22 00:17:46 +0300

    Implemented the elipse class

---
+++ TermPaper_OOP/Elipse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermPaper_OOP
+{
+    public class Elipse : VectorObject
+    {
+        public float Height { get; set; }
+        public float Width { get; set; }
+
+        public Elipse(float x, float y, float height, float width, Color color) 
+            : base(x, y, color)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public override float Perimeter()
+        {
+            return 2 * MathF.PI * MathF.Sqrt((MathF.Pow(Height, 2) + MathF.Pow(Width, 2)) / 2);
+        }
+
+        public override float Area()
+        {
+            return MathF.PI * (Height / 2) * (Width / 2);
+        }
+
+        public override void Draw(Graphics graph)
+        {
+            graph.DrawEllipse(Pen, X, Y, Width, Height);
+        }
+    }
+}
